d808831b9a3b557cf4fca00f3f688eec1247467f
Sztahovszky Dani <dsztahovszky@gmail.com>
2024-07-07 15:26:55 +0200

    tooltips and 'alone' option added

--- extentions/ask_two_options.py
+++ extentions/ask_two_options.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 
+try:
+    from custom_widgets import Tooltip
+except ModuleNotFoundError:
+    from extentions.custom_widgets import Tooltip
 
-def ttk_ask_two_options(question_text: str, button1_text: str, button2_text: str, question_font=...,
-                        parent: tk.Misc | None = None):
-    win = tk.Toplevel(parent)
+
+def ttk_ask_two_options(question_text: str, button1_text: str, button2_text: str, question_font=..., alone=False,
+                        b1_tooltip_text='', b2_tooltip_text='', parent: tk.Misc | None = None):
+    win = tk.Toplevel(parent) if not alone else tk.Tk()
     win.wm_overrideredirect(True)
 
     answer = tk.IntVar(win)
@@ -17,8 +22,12 @@ def ttk_ask_two_options(question_text: str, button1_text: str, button2_text: str
     btns_frame = tk.Frame(b_frame)
     option_1 = ttk.Button(btns_frame, text=button1_text, command=lambda: set_int(0))
     option_1.grid(column=0, row=0, ipadx=3, ipady=2)
+    if b1_tooltip_text != '':
+        Tooltip(option_1, b1_tooltip_text)
     option_2 = ttk.Button(btns_frame, text=button2_text, command=lambda: set_int(1))
     option_2.grid(column=1, row=0, ipadx=3, ipady=2)
+    if b2_tooltip_text != '':
+        Tooltip(option_2, b2_tooltip_text)
     btns_frame.pack(anchor=tk.CENTER)
     b_frame.pack()
 
