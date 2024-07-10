0f86fee05b0f6b259685702a6ef6df0a93c0a42d
Sztahovszky Dani <dsztahovszky@gmail.com>
2024-07-10 14:24:58 +0200

    update Message window in __init__()

--- extentions/ttk_ask.py
+++ extentions/ttk_ask.py
@@ -79,6 +79,7 @@ class Message:
         self.win.update_idletasks()
         self.win.geometry(f'+{int((self.win.winfo_screenwidth() / 2) - (self.win.winfo_width() / 2))}'
                           f'+{int((self.win.winfo_screenheight() / 2) - (self.win.winfo_height() / 2))}')
+        self.win.update()
 
     def show(self):
         self.win.mainloop()
